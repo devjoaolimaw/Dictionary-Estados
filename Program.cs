a95c458977fa80ca58b02b38dcb85fab833713c8
João Paulo <167019869+devjoaolimaw@users.noreply.github.com>
2024-06-03 15:51:46 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+Dictionary<string, string> estados = new Dictionary<string, string>();
+
+estados.Add("SP", "São Paulo");
+estados.Add("BA", "Bahia");
+estados.Add("MG", "Minas Gerais");
+
+Console.WriteLine(estados["MG"]);
+
+foreach(var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+estados.Remove("BA");
+estados["SP"] = "São Paulo - valor alterado";
+
+Console.WriteLine("-------------");
+
+foreach(var item in estados)
+{
+    Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+}
+
+string chave = "BA";
+Console.WriteLine($"Verificando o elemento: {chave}");
+
+if(estados.ContainsKey(chave))
+{
+    Console.WriteLine($"Valor existente:{chave}");
+}
+else
+{
+   Console.WriteLine($"Valor não exixtente. É seguro adicionar a chave: {chave}");
+}
